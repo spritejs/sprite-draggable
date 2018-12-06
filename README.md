43c676e90f3dcf8d1da610d8b8adf83abcde9b00
摇太阳 <yaotaiyang99@gmail.com>
2018-12-06 13:03:27 +0800

    补充说明文档，微调demo

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # sprite-draggable
 let sprite can draggable，让sprite对象简单得拥有draggable的能力
+
+### 安装
+
+```
+npm install 
+
+npm start
+```
+访问 http://localhost:9092 查看具体demo
+
+
+
+### 使用
+```
+  import { draggable } from 'sprite-draggable'
+
+  …
+
+  let group = draggable(new Group());
+
+  let sprite = draggable(new Sprite());
+
+  //表示在[0,0]与[300,300]这两点矩形之间拖动[xmin,ymin,xmax,ymax]，不设置表示不控制拖动范围 
+  sprite.dragRect = [0,0,300,300]; 
+
+  //表示拖动的范围大于坐标[0,0]
+  //sprite.dragRect = [0,0]; 
+
+  /**拖动过程中，有三个事件 dragstart、dragging、dragend**/
+  sprite.on('dragstart',function(event){
+    console.log('dragging');
+  });
+
+  sprite.on('dragging',function(event){
+    console.log('dragging');
+  });
+
+  sprite.on('dragend',function(event){
+    console.log('dragend');
+  });
+
+```
+
+
--- example/index.js
+++ example/index.js
@@ -4,20 +4,26 @@ import { ResizeBlock } from './ResizeBlock'
 let i = 0;
 const { Scene, Sprite, Group } = spritejs, w = window.innerWidth, h = window.innerHeight;
 const scene = new Scene('#canvas-wrap', { viewport: [ w, h ], displayRatio: 'auto', resolution: 'flex' });
-scene.delegateEvent('mousewheel', document);
+
+scene.delegateEvent('mousewheel', document); //sprite 元素侦听mousewheel事件
+
 const layer = scene.layer();
 let child = draggable(new Group());
 let child1 = new Sprite();
 let child2 = new ResizeBlock({ size: [ 100, 30 ], backgroundColor: '#eee', dragRect: [ 0, 0 ] });
 child.attr({ size: [ 100, 30 ], bgcolor: '#f00', pos: [ 200, 200 ] })
 child1.attr({ size: [ 100, 30 ], pos: [ 300, 300 ], bgcolor: '#0f0' })
-child.dragRect = [ 0, 0, 300, 300 ];
+
+child.dragRect = [ 0, 0, 300, 300 ];//设置拖动范围
+
 let group = draggable(new Group());
 group.attr({ size: [ w, h ], bgcolor: '#ff0', rotate: 0, pos: [ w / 2, h / 2 ] });
+
 group.append(child2);
 group.append(child1);
 group.append(child);
 layer.append(group);
+
 group.on('mousewheel', (e) => {
   e.preventDefault();
   const [ scaleX, scaleY ] = group.attr('scale');
@@ -27,6 +33,8 @@ group.on('mousewheel', (e) => {
     direction = -1;
   }
   const dscale = 0.05 * direction;
+
+  /** 计算以鼠标点为中心缩放 **/
   const [ oAnchorX, oAnchorY ] = group.attr('anchor');
   let pX = (oAnchorX * w + e.offsetX) / w; //鼠标点相对占比
   let pY = (oAnchorY * h + e.offsetY) / h;
